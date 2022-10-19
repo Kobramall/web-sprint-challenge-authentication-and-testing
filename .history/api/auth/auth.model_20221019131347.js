2f6176b114b5e17bda1b27026dfc1b367aa56bdd
guest hugh <kbramall11@gmail.com>
2022-10-19 13:18:05 -0600

    I add the login function

---
+++ .history/api/auth/auth.model_20221019131347.js
@@ -0,0 +1,15 @@
+const db = require('../../data/dbConfig')
+
+async function add(user){
+    const [id] = await db('users').insert(user)
+    return db('users').where('id', id).first()
+}
+
+function findBy(user){
+    return db('users').where('username', user)
+}
+
+module.exports = {
+    add, 
+    findBy
+}
